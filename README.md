b90350336f5b6515ab7ef0ccad3168670466124c
Ilan <ilan.pillemer@gmail.com>
2020-04-11 17:11:20 +0100

    wrangle data

--- README.md
+++ README.md
@@ -1,3 +1,20 @@
 # Sources
 
 https://github.com/CSSEGISandData/COVID-19
+
+# get data
+
+curl https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv
+
+# ideas
+https://d3js.org
+https://github.com/d3/d3/blob/master/API.md
+https://github.com/d3/d3/wiki/Tutorials
+https://bl.ocks.org
+https://observablehq.com/@d3/multi-line-chart
+
+#d3
+https://d3js.org/d3.v5.min.js
+
+https://d3js.org/d3-array.v2.min.js
+https://github.com/d3/d3-array
--- cmd/server/data/corona.js
+++ cmd/server/data/corona.js
@@ -1,8 +1,18 @@
 function render() {
   console.log("starting rendering")
-  d3.csv("deaths.csv", data => {vis(data)} )
+  d3.csv("deaths.csv", data => {return {data}}).then(data => vis(data))
 }
 
-function vis(data) {
- console.log(data)
+function vis(d) {
+  // turn the data into a what d3 expects for time series
+  const columns = d.columns.slice(4);
+  var data = {
+    y: "confirmed deaths",
+    series: d.map(e => ({
+      name: e.data["Country/Region"] + " " + e.data["Province/State"],
+      values: columns.map(k => +e.data[k])
+    })),
+   dates: columns.map(d3.utcParse("%M/%d/%y"))
+  }
+  console.log(data)
 }
